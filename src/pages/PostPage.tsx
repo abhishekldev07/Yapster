347c9fa0f5f9a6e70a630d532ea590f173bfac39
Abhishek Lamichhane <abhishek.l.dev07@gmail.com>
2026-09-05 11:20:05 +0545

    align post page with Yapster app shell

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -3,9 +3,12 @@ import { PostDetail } from "../components/PostDetail";
 
 export const PostPage = () => {
   const { id } = useParams<{ id: string }>();
+
   return (
-    <div className="pt-20">
-      <PostDetail postId={Number(id)} />
-    </div>
+    <main className="pb-16 pt-7 max-[760px]:pb-8 max-[760px]:pt-4">
+      <div className="mx-auto max-w-[980px] px-4 sm:px-6">
+        <PostDetail postId={Number(id)} />
+      </div>
+    </main>
   );
 };
