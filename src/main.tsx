09edf21cbbb36086483f93a843041302482693c8
Abhishek Lamichhane <abhishek.l.dev07@gmail.com>
2026-09-06 15:35:37 +0545

    load final phase 4 review overrides

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "./theme.css";
 import "./phase2-polish.css";
 import "./phase4-polish.css";
+import "./phase4-final-overrides.css";
 
 import { BrowserRouter as Router } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
